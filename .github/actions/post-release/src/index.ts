e4e70953aa5347f933a71000cd45f5f6d3df3dc0
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2026-02-28 09:36:58 +0000

    try ci

--- .github/actions/post-release/src/index.ts
+++ .github/actions/post-release/src/index.ts
@@ -18,69 +18,71 @@ run();
 async function run() {
   try {
     const token = String(process.env.ACCESS_TOKEN);
-    const releaseId = Number(process.env.RELEASE_ID);
+    const releaseId = Number('291437629');
+    // const releaseId = Number(process.env.RELEASE_ID);
 
     const octokit = getOctokit(token);
 
     const releaseAssets = await octokit.request(
       `GET /repos/${owner}/${repo}/releases/${releaseId}/assets`,
     );
-    const all = releaseAssets.data as Asset[];
-    const sigAssets = all.filter((asset) => asset.name.endsWith('.sig'));
-    const assets = all.filter((asset) => !asset.name.endsWith('.sig'));
+    console.log(releaseAssets);
+    // const all = releaseAssets.data as Asset[];
+    // const sigAssets = all.filter((asset) => asset.name.endsWith('.sig'));
+    // const assets = all.filter((asset) => !asset.name.endsWith('.sig'));
 
-    console.log('removing .sig assets...');
-    await Promise.all(
-      sigAssets.map((asset) =>
-        octokit.rest.repos.deleteReleaseAsset({
-          owner,
-          repo,
-          asset_id: asset.id,
-        }),
-      ),
-    );
+    // console.log('removing .sig assets...');
+    // await Promise.all(
+    //   sigAssets.map((asset) =>
+    //     octokit.rest.repos.deleteReleaseAsset({
+    //       owner,
+    //       repo,
+    //       asset_id: asset.id,
+    //     }),
+    //   ),
+    // );
 
-    console.log('adding labels...');
-    await Promise.all(
-      assets
-        .reduce((acc: AssetWithLabel[], asset: Asset) => {
-          const { name } = asset;
+    // console.log('adding labels...');
+    // await Promise.all(
+    //   assets
+    //     .reduce((acc: AssetWithLabel[], asset: Asset) => {
+    //       const { name } = asset;
 
-          if (name.endsWith('.rpm')) {
-            acc.push({ ...asset, label: 'ISOS for Linux (rpm)' });
-          }
-          if (name.endsWith('.deb')) {
-            acc.push({ ...asset, label: 'ISOS for Linux (deb)' });
-          }
-          if (name.endsWith('.exe')) {
-            acc.push({ ...asset, label: 'ISOS for Windows' });
-          }
-          if (name.endsWith('aarch64.dmg')) {
-            acc.push({ ...asset, label: 'ISOS for Mac (Apple Silicon)' });
-          }
-          if (name.endsWith('x64.dmg')) {
-            acc.push({ ...asset, label: 'ISOS for Mac (Intel)' });
-          }
+    //       if (name.endsWith('.rpm')) {
+    //         acc.push({ ...asset, label: 'ISOS for Linux (rpm)' });
+    //       }
+    //       if (name.endsWith('.deb')) {
+    //         acc.push({ ...asset, label: 'ISOS for Linux (deb)' });
+    //       }
+    //       if (name.endsWith('.exe')) {
+    //         acc.push({ ...asset, label: 'ISOS for Windows' });
+    //       }
+    //       if (name.endsWith('aarch64.dmg')) {
+    //         acc.push({ ...asset, label: 'ISOS for Mac (Apple Silicon)' });
+    //       }
+    //       if (name.endsWith('x64.dmg')) {
+    //         acc.push({ ...asset, label: 'ISOS for Mac (Intel)' });
+    //       }
 
-          return acc;
-        }, [])
-        .map(({ id, label }) =>
-          octokit.rest.repos.updateReleaseAsset({
-            owner,
-            repo,
-            asset_id: id,
-            label,
-          }),
-        ),
-    );
+    //       return acc;
+    //     }, [])
+    //     .map(({ id, label }) =>
+    //       octokit.rest.repos.updateReleaseAsset({
+    //         owner,
+    //         repo,
+    //         asset_id: id,
+    //         label,
+    //       }),
+    //     ),
+    // );
 
-    console.log('publishing release...');
-    await octokit.rest.repos.updateRelease({
-      owner,
-      repo,
-      release_id: releaseId,
-      draft: false,
-    });
+    // console.log('publishing release...');
+    // await octokit.rest.repos.updateRelease({
+    //   owner,
+    //   repo,
+    //   release_id: releaseId,
+    //   draft: false,
+    // });
   } catch (error) {
     setFailed(error as Error);
   }
